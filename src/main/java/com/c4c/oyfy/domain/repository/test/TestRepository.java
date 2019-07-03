68656a122772d14359a64336416dde9b6f42b7a9
Matsumoto Masayoshi <matsumoto.develop@gmail.com>
2019-07-03 20:06:45 +0900

    とりあえず

--- src/main/java/com/c4c/oyfy/domain/repository/test/TestRepository.java
+++ src/main/java/com/c4c/oyfy/domain/repository/test/TestRepository.java
@@ -1,6 +1,12 @@
 package com.c4c.oyfy.domain.repository.test;
 
+import java.util.List;
+
+import com.oyfy.dbflute.exentity.Bath;
+
 public interface TestRepository {
 
     public int test();
+
+    public List<Bath> searchPathList(String keyword);
 }
--- src/main/java/com/c4c/oyfy/domain/repository/test/TestRepositoryImpl.java
+++ src/main/java/com/c4c/oyfy/domain/repository/test/TestRepositoryImpl.java
@@ -1,9 +1,12 @@
 package com.c4c.oyfy.domain.repository.test;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.oyfy.dbflute.exbhv.BathBhv;
+import com.oyfy.dbflute.exentity.Bath;
 
 
 @Repository
@@ -23,5 +26,13 @@ public class TestRepositoryImpl implements TestRepository {
             cb.query().setBath24hFlg_Equal(1);
         });
 
+    }
+
+    @Override
+    public List<Bath> searchPathList(String keyword) {
+        List<Bath> bath =  BathBhv.selectList(cb -> {
+            cb.query().setBathFee_GreaterEqual(460);
+        });
+        return bath;
     }
 }
