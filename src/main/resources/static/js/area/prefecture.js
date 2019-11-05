075a4e9e8d991394a240dd80947792e6b266e9c6
tachikawa <hidamari.3850@gmail.com>
2019-11-05 23:16:03 +0900

    地域名検索画面のレイアウト調整

--- src/main/resources/static/js/area/prefecture.js
+++ src/main/resources/static/js/area/prefecture.js
@@ -25,3 +25,20 @@ $(function() {
 
     });
 });
+
+$(function() {
+        $("a.switch-btn").on('click', function() {
+        $("li.area-name-list").toggle("fast",alertFunc);
+});
+
+function alertFunc(){
+    if($("li.area-name-list").css('display') == 'none') {
+        $("#all").hide();
+        $("#btn-inner").text("▲");
+    }
+    else{
+        $("#all").show();
+        $("#btn-inner").text("▼");
+    }
+  };
+});
--- src/main/resources/static/js/area/station.js
+++ src/main/resources/static/js/area/station.js
@@ -4,10 +4,23 @@
 function searchStation() {
     const targetTable = document.getElementById('search_station');
 
+    var targetDocument = document.getElementsByClassName('line-name');
+    var targetCheckBox = targetDocument[0].getElementsByTagName("input");
+
     for (var i = 0, rowLen = targetTable.rows.length; i < rowLen; i++) {
 
+        var targetTr = targetTable.rows[i];
+        targetTr.classList.add('stationsRow' + i);
+
+        // 路線列のtdのnameを設定
         // tr内のtdをループ。cellsコレクションで行内セル位置取得。
         for (var j = 0, colLen = targetTable.rows[i].cells.length; j < colLen; j++) {
+
+            // 駅名を出力するtdに対してクラス名を追加
+            if (j = 1) {
+                var targetTd = targetTable.rows[i].cells[j];
+                targetTd.classList.add('stations' + i);
+            }
 
             // tr直下の子要素（路線名が表示されるtd）
             var trObject = $(".stationsRow" + i).children('[id=all]');
@@ -38,9 +51,9 @@ function getClassName(n) {
     var targetTable = document.getElementById('search_station');
     var targetClassName = document.getElementsByClassName('stations' + n);
 
-    var findClassName = targetClassName[0].getAttribute("class", "stations0");
+//    var findClassName = targetClassName[0].getAttribute("class", "stations0");
 
-    return findClassName;
+//    return findClassName;
 };
 
 //tableのrowの数を取得
@@ -61,29 +74,29 @@ $(function() {
             // 駅名を出力するtdに対してクラス名を追加
             if (j = 1) {
                 var targetTd = targetTable.rows[i].cells[j];
-                targetTd.classList.add('stations' + i);
+//                targetTd.classList.add('stations' + i);
             }
 
             // tr直下の子要素（路線名が表示されるtd）
             var trObject = $(".stationsRow" + i).children('[id=all]');
 
             // trObject配下にある子要素に存在するinput要素
-            var inputObject = trObject.prevObject[0].children[0]
-                    .getElementsByTagName("input");
-
-            inputObject.lineCheck.setAttribute("name", "stations" + i);
+//            var inputObject = trObject.prevObject[0].children[0]
+//                    .getElementsByTagName("input");
+//
+//            inputObject.lineCheck.setAttribute("name", "stations" + i);
 
             // trObject配下にある2番目の子要素に存在するinput要素
-            var stationRow = trObject.prevObject[0].children[1]
-                    .getElementsByTagName("input");
+//            var stationRow = trObject.prevObject[0].children[1]
+//                    .getElementsByTagName("input");
 
             // 駅名checkboxの数を取得
-            var checkboxLow = stationRow.length;
+//            var checkboxLow = stationRow.length;
 
             // checkbox分ループ
-            for (var k = 0, checkboxLow; k < checkboxLow; k++) {
-                stationRow[k].setAttribute("name", "stations" + i);
-            }
+//            for (var k = 0, checkboxLow; k < checkboxLow; k++) {
+//                stationRow[k].setAttribute("name", "stations" + i);
+//            }
 
         }
     }
@@ -99,21 +112,21 @@ $(function() {
         agumentName = allCheckerName.substr(-1, 1);
 
         // 対象のtdクラス名を取得する
-        var tdName = getClassName(agumentName);
+//        var tdName = getClassName(agumentName);
 
-        targetCheckBox = 'input[name=' + '"' + tdName + '"' + ']'
+//        targetCheckBox = 'input[name=' + '"' + tdName + '"' + ']'
 
         // もし「全てにチェック」のチェックが入ったら
         if ($(this).prop('checked')) {
 
             // チェックを付ける
-            $(targetCheckBox).prop('checked', true);
+//            $(targetCheckBox).prop('checked', true);
 
             // もしチェックが外れたら
         } else {
 
             // チェックを外す
-            $(targetCheckBox).prop('checked', false);
+//            $(targetCheckBox).prop('checked', false);
         }
 
     });
