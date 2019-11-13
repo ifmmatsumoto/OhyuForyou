4df9f940f01636f6cd1d7d6a4827a20a8d9d78ef
haraguchi <n.haraguchi@c4c.co.jp>
2019-11-13 15:06:51 +0900

    おじじにクリックアクションを追加

--- src/main/resources/static/js/top/top.js
+++ src/main/resources/static/js/top/top.js
@@ -1,12 +1,26 @@
+// 現在地情報取得
 function getCuurentLocation() {
     navigator.geolocation.getCurrentPosition(searchNearnyBath);
 }
+// 現在地検索
 function searchNearnyBath(position) {
     document.getElementById('latitude').value = position.coords.latitude;
     document.getElementById('longitude').value = position.coords.longitude;
 
     $('#cuurentLocationSearch').submit();
 }
+// キーワード検索
 function searchKeyword() {
     $('#keywordSearch').submit();
+}
+// おじじ画像切り替え
+function changeOjiji(img) {
+	var ojiji1 = document.getElementsByClassName("ojiji1")[0];
+	var ojiji2 = document.getElementsByClassName("ojiji2")[0];
+	img.style.display = 'none';
+	if(img.className == "ojiji1") {
+		ojiji2.style.display = 'block';
+	} else {
+		ojiji1.style.display = 'block';
+	}
 }
