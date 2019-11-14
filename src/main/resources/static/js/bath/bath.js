476c69b57c60e377561c54a7788bff5bbe43d8ff
haraguchi <n.haraguchi@c4c.co.jp>
2019-11-14 11:59:08 +0900

    レビュー一覧画面側

--- src/main/resources/static/js/bath/bath.js
+++ src/main/resources/static/js/bath/bath.js
@@ -1,24 +1,31 @@
 var map;
 function initMap() {
-  // 地図情報の取得
-  var gmap = document.getElementById('gmap');
-  var bath_lat  = document.getElementById('lat').value;
-  var bath_lon  = document.getElementById('lon').value;
+	// 地図情報の取得
+	var gmap = document.getElementById('gmap');
+	var bath_lat  = document.getElementById('lat').value;
+	var bath_lon  = document.getElementById('lon').value;
 
-  // マップに反映
-  var mapPosition = {lat: Number(bath_lat), lng: Number(bath_lon)};
-  map = new google.maps.Map(gmap, { center: mapPosition, zoom: 14});
+	// マップに反映
+	var mapPosition = {lat: Number(bath_lat), lng: Number(bath_lon)};
+	map = new google.maps.Map(gmap, { center: mapPosition, zoom: 14});
 
-  // マーカーを設定
-  var markerOptions = {
-		  map: map,
-		  position: mapPosition,
-          icon: new google.maps.MarkerImage(
-        		  '/oyfy/img/ojiji/ojiji_pin.png',
-        		  new google.maps.Size(80,50),
-		          new google.maps.Point(0,0),
-		          new google.maps.Point(80,25)
-		  )
-  };
-  var marker = new google.maps.Marker(markerOptions);
+	// マーカーを設定
+	var markerOptions = {
+			map: map,
+			position: mapPosition,
+			icon: new google.maps.MarkerImage(
+					'/oyfy/img/ojiji/ojiji_pin.png',
+					new google.maps.Size(80,50),
+					new google.maps.Point(0,0),
+					new google.maps.Point(80,25)
+			)
+	};
+	var marker = new google.maps.Marker(markerOptions);
+}
+
+// レビュー投稿
+function registReview() {
+	if(window.confirm('レビューを投稿します。よろしいですか？')) {
+		document.bathForm.submit();
+	}
 }
